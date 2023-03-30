6cad035ff2899c5791456f8199a15dda2d796301
Vlad <gold.craft.geme@gmail.com>
2023-03-30 20:42:33 +0300

    build: parameters moved to variables

--- setup.py
+++ setup.py
@@ -8,50 +8,37 @@ def _get_variable_from_init_file(variable_name: str) -> str:
         return re.search(pattern, file.read(), re.MULTILINE).group(1)
 
 
-def get_version():
-    return _get_variable_from_init_file("__version__")
-
-
-def get_name():
-    return _get_variable_from_init_file("__title__")
-
-
-def get_author():
-    return _get_variable_from_init_file("__author__")
-
-
-def get_license():
-    return _get_variable_from_init_file("__license__")
-
-
-def get_description():
-    return _get_variable_from_init_file("__description__")
-
-
-def get_long_description():
+def get_long_description() -> str:
     with open("README.md") as file:
         return file.read()
 
 
-def get_requirements():
+def get_requirements() -> list:
     with open("requirements.txt") as file:
         return file.read().splitlines()
 
 
+NAME = _get_variable_from_init_file("__title__")
+VERSION = _get_variable_from_init_file("__version__")
+AUTHOR = _get_variable_from_init_file("__author__")
+DESCRIPTION = _get_variable_from_init_file("__description__")
+LONG_DESCRIPTION = get_long_description()
+REQUIREMENTS = get_requirements()
+
 setup(
-    name=get_name(),
+    name=NAME,
     url="https://github.com/GoldMasterPro/nz-ua",
     project_urls={
         'Documentation': 'https://GoldMasterPro.github.io/nz-ua',
     },
-    version=get_version(),
-    author=get_author(),
-    description=get_description(),
-    license=get_license(),
+    version=VERSION,
+    author=AUTHOR,
+    description=DESCRIPTION,
+    license="MIT",
     packages=find_packages(),
-    long_description=get_long_description(),
+    long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
-    install_requires=get_requirements(),
+    install_requires=REQUIREMENTS,
     python_requires='>=3.10.0',
     keywords=[
         "nz",
